d73b8d5b837c575d02a8acb3754f2fd1c4746700
VitorD <vitodaltro8@gmail.com>
2021-07-27 15:57:31 -0300

    add image sunday e ajustes home

--- src/FoccoEmFrente.Kanban.Web/ClientApp/src/components/Home/home.js
+++ src/FoccoEmFrente.Kanban.Web/ClientApp/src/components/Home/home.js
@@ -1,9 +1,8 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+
+
+
 
 export default function Home() {
-   return (
-      <div>
-         <p>Home</p>
-      </div>
-   );
+return <div><p>Home</p></div> 
 }
--- src/FoccoEmFrente.Kanban.Web/ClientApp/src/components/Layout/layout.js
+++ src/FoccoEmFrente.Kanban.Web/ClientApp/src/components/Layout/layout.js
@@ -1,5 +1,15 @@
 import React from "react";
+import Logo from '../../assets/logo.png'
 
 export default function Layout(props) {
-   return <div>{props.children}</div>;
+   return (
+      <div className="form-container">
+         <img src ={Logo} alt="Sunday"/>
+         <div className ="content">
+            {props.children}
+         </div>
+      </div>
+
+
+   )
 }
