ff885cd826264cc3cd3f80b8a569ccac49d5274e
VitorD <vitodaltro8@gmail.com>
2021-07-27 15:39:45 -0300

    unitOfWork e add CommitAsync

--- src/FoccoEmFrente.Kanban.Application/Context/KanbanContext.cs
+++ src/FoccoEmFrente.Kanban.Application/Context/KanbanContext.cs
@@ -1,11 +1,13 @@
 ﻿using FoccoEmFrente.Kanban.Application.Entities;
 using FoccoEmFrente.Kanban.Application.Mapping;
+using FoccoEmFrente.Kanban.Application.Repositories;
 using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
+using System.Threading.Tasks;
 
 namespace FoccoEmFrente.Kanban.Application.Context
 {
-    public class KanbanContext : IdentityDbContext
+    public class KanbanContext : IdentityDbContext, IUnitOfWork
     {
         public KanbanContext(DbContextOptions<KanbanContext> options) : base(options)
         {
@@ -13,6 +15,11 @@ namespace FoccoEmFrente.Kanban.Application.Context
         }
 
         public DbSet<Activity> Activities { get; set; }
+
+        public async Task<bool> CommitAsync()
+        {
+            return await SaveChangesAsync() > 0;
+        }
 
         protected override void OnModelCreating(ModelBuilder modelBuilder)
         {
