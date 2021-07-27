e0974498fac6e1613df00c6ecd02f0b91b788ab0
VitorD <vitodaltro8@gmail.com>
2021-07-27 15:58:24 -0300

    componentes login

--- src/FoccoEmFrente.Kanban.Web/ClientApp/src/components/Login/login.js
+++ src/FoccoEmFrente.Kanban.Web/ClientApp/src/components/Login/login.js
@@ -1,9 +1,61 @@
 import React from "react";
+function Botao({onVoltar}){
+   return (
+      <>
+      <button className="btn btn-primary" type="submit">Entrar</button>
+      <button className="btn btn-secondary" onClick={onVoltar}>Voltar</button>
+      </>
+   )
+}
+function Senha(){
+   return (
+      <>
+      <label htmlFor="Senha">Senha</label>
+      <input id="senha" type="password" placeholder ="Senha"></input>
+           
+      </>
+   )
 
-export default function Login() {
+}
+function Email(){
+
+   return (
+      <>
+      <label htmlFor="email">Email</label>
+      <input id="email" type="email" placeholder ="E-mail"></input>
+             
+      </>
+   )
+}
+function Paragrafo(){
+   return <p>Bem vindo ao <strong>Sunday.com</strong>, melhor sistema para gestao de tasks</p>
+
+}
+function Div(){}
+function Macro(variavel){
+   return(<>
+
+      <div style = {{width: "450px"}}>
+               <form>
+                  {variavel}
+               </form>
+            </div>
+         </>
+         )
+
+}
+export default function Login({history}) {
+   
+   const onVoltar = () =>
+   {
+      history.push("/");
+   }
+
    return (
       <div>
-         <p>Login</p>
+         <Macro variavel={Email}/>
       </div>
+      
+   
    );
 }
